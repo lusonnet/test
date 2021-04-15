8d2304598135abe196fe5f6ea2cc716dc5fcc4c7
sonnet <lulu_2001@foxmail.com>
2021-04-15 23:14:24 +0800

    提交代码

---
+++ 4_15/4_15/test.cpp
@@ -0,0 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	char arr[] = {'a','b','c'};
+//	printf("%d", strlen(arr));
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int n = 10;
+//	int arr[n] = { 0 };
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1:m++;
+//	case 2:n++;
+//	case 3:
+//		switch (n)
+//		{
+//		case 1:
+//			n++;
+//		case 2:m++; n++;
+//			break;
+//		}
+//	case 4:
+//		m++;
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("m = %d,n = %d\n", m, n);
+//	return 0;
+//}
+
+//int mian()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	}
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	//getchar - ctrl+z 读取结束
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码：");
+//	scanf("%s", password);
+//	printf("请确认密码（Y/N):");
+//	int tmp = 0;
+//	while ((tmp = getchar()) != '\n');
+//	{
+//		;
+//	}
+//	int ch = getchar();
+//	if (ch == 'Y');
+//	{
+//		printf("确认成功\n");
+//	}
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10;)
+//	{
+//		printf("%d ", i);
+//		i++
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	for (;;)
+//	{
+//		printf("hehe");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 3; i++)
+//	{
+//		for (; j < 3; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)
+//			printf("%d", i);
+//	}
+//	return 0;
+//}
