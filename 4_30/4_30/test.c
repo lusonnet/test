77805f89a4542e3ff438045ea6463db150a80dbd
sonnet <lulu_2001@foxmail.com>
2021-05-04 23:32:18 +0800

    提交代码

---
+++ 4_30/4_30/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	float a,d;
+//	int b;
+//	char c;
+//	scanf("%f %d %c %f", &a, &b, &c, &d);
+//	printf("%c %d %.2f %.2f", c, b, a, d);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d:%d", &a, &b);
+//	if (a > 12)
+//	{
+//		printf("%d:%d PM", a - 12, b);
+//	}
+//	else if (a == 12)
+//	{
+//		printf("%d:%d PM", a, b);
+//	}
+//	else
+//	{
+//		printf("%d:%d AM",a,b );
+//	}
+//	return 0;
+//}
+
+//int mian()
+//{
+//	int speed;
+//	scanf("%d", &speed);
+//	if (speed > 60)
+//		printf("Speed: %d - Speeding",speed);
+//	else
+//		printf("Speed: %d - OK",speed);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a == b)
+//		printf("C", c);
+//	if (a == c)
+//	{
+//		printf("B", b);
+//	}
+//	if (b == c)
+//	{
+//		printf("A", a);
+//	}
+//	return 0;
+//}
