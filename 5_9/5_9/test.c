61df30e71feb06726c21a5bfd4c9f6ffddaecd45
sonnet <lulu_2001@foxmail.com>
2021-05-09 22:35:22 +0800

    提交代码

---
+++ 5_9/5_9/test.c
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	for (int i = 31; i >=1; i-=2)
+//	{
+//		if ((a >> i) & 1 == 1)
+//		{
+//			printf("1");
+//		}
+//		else
+//		{
+//			printf("0");
+//		}
+//	}
+//	printf("\n");
+//	for (int i = 30; i >= 0; i -= 2)
+//	{
+//		if ((a >> i) & 1 == 1)
+//		{
+//			printf("1");
+//		}
+//		else
+//		{
+//			printf("0");
+//		}
+//	}
+//	return 0;
+//}
+
+//int f(int x)
+//{
+//	int count = 0;
+//	int a;
+//	scanf("%d", &a);
+//	if (a >= 0)
+//	{
+//		for (int i = 1; i <= 32; i++)
+//		{
+//			if ((a >> i) & 1 == 1)
+//			{
+//				count++;
+//			}
+//		}
+//	}
+//	return count;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int count = 0;
+//	int a;
+//	scanf("%d", &a);
+//	if (a >= 0)
+//	{
+//		for (int i = 1; i <= 32; i++)
+//		{
+//			if ((a >> i) & 1 == 1)
+//			{
+//				count++;
+//				//break;
+//			}
+//		}
+//		printf("%d", count);
+//	}
+//	else
+//	{
+//		for (int j = 32; j > 0; j--)
+//		{
+//			if ((a << j) & 1 != 0)
+//			{
+//				printf("1");
+//			}
+//			else
+//				printf("0");
+//		}
+//	}
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	printf("a = %d b = %d", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a = %d b = %d", a, b);
+//	return 0;
+//}
+
+#include<stdio.h>
+//int main()
+//{
+//	char str[] = "hello world";
+//	printf("%d %d\n", sizeof(str), strlen(str));
+//	return 0;
+//}
+
+int main()
+{
+	
+}
