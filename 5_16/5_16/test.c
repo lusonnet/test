22b07c8f2fd0bbc07870a6dda35f6f58e36de643
sonnet <lulu_2001@foxmail.com>
2021-05-16 23:48:45 +0800

    提交代码

---
+++ 5_16/5_16/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	char a = 3;
+//	char b = 127;
+//	char c = a + b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = 0xb6;
+//	short b = 0xb600;
+//	int c = 0xb6000000;
+//	if (a == 0xb6)
+//		printf("a");
+//	if (b == 0xb600)
+//		printf("b");
+//	if (c == 0xb6000000)//c本身就是整型，不会发生整型提升
+//		printf("c");
+//	return 0;
+//}
+
+//int main()
+//{
+//	char c = 1;
+//	printf("%u\n", sizeof(c));//1
+//	printf("%u\n", sizeof(+c));//4
+//	printf("%u\n", sizeof(-c));//4
+//	printf("%u\n", sizeof(!c));//4
+//}
+
+int main()
+{
+
+	return 0;
+}
