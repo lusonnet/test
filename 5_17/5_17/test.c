181dc33230ce7195bacb2f13e3a73204038b7cad
sonnet <lulu_2001@foxmail.com>
2021-05-17 23:15:20 +0800

    提交代码

---
+++ 5_17/5_17/test.c
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+	//int a = 4;
+	//float f = 4.5f;
+	//a + f;
+
+	//a * b + c * d + e * f;
+
+//	c + --c;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 10;
+//	i = i-- - --i * i++ + ++i;
+//	printf("i = %d\n", i);
+//	return 0;
+//}
+
+//int fun()
+//{
+//	static int count = 1;
+//	return ++count;
+//}
+//
+//int main()
+//{
+//	int answer;
+//	answer = fun() - fun() * fun();
+//	printf("%d\n", answer);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int ret = (++i) + (++i) + (++i);
+//	printf("%d\n", ret);
+//	printf("%d\n", i);
+//	return 0;
+//}
+//#include<string.h>
+//int main()
+//{
+//	char str[101];
+//	gets(str);
+//	int sz = strlen(str);
+//
+//	int left, right;
+//	left = 0;
+//	right = sz - 1;
+//	while (left < right)
+//	{
+//		int t = str[left];
+//		str[left] = str[right];
+//		str[right] = t;
+//		left++;
+//		right--;
+//	}
+//	puts(str);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j = 0;
+//	for (i = 0; i < 7; i++)
+//	{
+//		for (j = 0; j < 7 - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 6; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * (6 - i)-1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+	int sum = 18;
+	int cola = 3;//Æ¿Êý
+	int	cost = 0;
+
+	return 0;
+}
