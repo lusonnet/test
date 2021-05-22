8e5aca4f5adf8632d4f2fa11c3c3590bdaf6b225
sonnet <lulu_2001@foxmail.com>
2021-05-22 23:55:16 +0800

    提交代码

---
+++ 5_22/5_22/test.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+	//for (i = 0; i < sz; i++)
+	//{
+	//	printf("%d\n", arr[i]);
+	//}
+
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 1;
+//	int sum = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
