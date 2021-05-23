d77e9314767e6e58eb2936a60c0b7acec01d2273
sonnet <lulu_2001@foxmail.com>
2021-05-23 23:33:32 +0800

    tijiao

---
+++ 5_23-1/5_23-1/test.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<assert.h>
+
+//while (*src != '\0')
+//{
+//	*dst = *src;
+//	dst++;
+//	src++;
+//}
+//*dst = *src;
+
+//while (*src != '\0')
+//{
+//	*dst++ = *src++;
+//}
+//*dst = *src;
+
+
+//char* my_strcpy(char* dst, const char* src)//‘§∑¿∑¥øΩ±¥
+//{
+//
+//	assert(src != NULL);//∂œ—‘ - ‘§∑¿ø’÷∏’Î
+//	assert(dst != NULL);//∂œ—‘ - ‘§∑¿ø’÷∏’Î
+//	char* ret = dst;
+//	while (*dst++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+
+//int main()
+//{
+//	char arr1[20] = "xxxxxxxxxx";
+//	char arr2[] = "hello";
+//	my_strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//	return 0;
+//}
+//
+//const int* p = &num;
+
+//int my_strlen(const char* arr)
+//{
+//	assert(arr != NULL);
+//	int count = 0;
+//	while (*arr++ != '\0')
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abc";
+//	int len = my_strlen(arr);
+//	printf("%d", len);
+//	return 0;
+//}
+
+
+//int check_sys()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	return *p;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//	{
+//		printf("–°∂À");
+//	}
+//	if (ret == 0)
+//	{
+//		printf("¥Û∂À");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("%d %d %d", a, b, c);//%d - –Ë’˚–ÕÃ·…˝
+//}
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+int main()
+{
+	char a = 128;
+	char a = 128;
+	printf("%u\n", a);
+	return 0;
+}
