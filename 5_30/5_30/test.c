8c882cb876334900a36d56b8bbe4e6053ab3c753
sonnet <lulu_2001@foxmail.com>
2021-05-31 23:43:00 +0800

    提交代码

--- 5_30/5_30/test.c
+++ 5_30/5_30/test.c
@@ -2,38 +2,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void print_arr(int arr[], int sz)
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-}
-
+//void print_arr(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
 int cmp_int(const void* e1, const void* e2)
 {
 	return *(int*)e1 - *(int*)e2;//强制类型转化为整型指针 - 升序
 	//return *(int*)e2 - *(int*)e1;//强制类型转化为整型指针 - 降序
 }
-
-void test1()
-{
-	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	qsort(arr, sz, sizeof(arr[0]), cmp_int);
-	print_arr(arr, sz);
-	return 0;
-
-}
-
+//
+//void test1()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//	print_arr(arr, sz);
+//	return 0;
+//
+//}
+//
 struct Stu
 {
 	char name[20];
 	int age;
 };
-
+//
 int sort_age(const void* e1,const void* e2)
 {
 	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
@@ -43,16 +43,28 @@ int sort_name(const void* e1, const void* e2)
 {
 	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
 }
-void test2()
+//void test2()
+//{
+//	struct Stu s[] = { {"zhang",30},{"li",34},{"wan",20} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), sort_age);//年龄
+//	qsort(s, sz, sizeof(s[0]), sort_name);//名字
+//	//print_arr(s, sz);
+//
+//}
+
+Swap(char* buf1, char* buf2, int width)
 {
-	struct Stu s[] = { {"zhang",30},{"li",34},{"wan",20} };
-	int sz = sizeof(s) / sizeof(s[0]);
-	qsort(s, sz, sizeof(s[0]), sort_age);//年龄
-	qsort(s, sz, sizeof(s[0]), sort_name);//名字
-	//print_arr(s, sz);
-
+	int i = 0;
+	for (i = 0; i < width; i++)//width - 宽度，比较每一个字节
+	{
+		char t = *buf1;
+		*buf1 = *buf2;
+		*buf2 = t;
+		buf1++;
+		buf2++;
+	}
 }
-
 void bubble_sort(void* base,
 				int sz, 
 				int width, //未知类型
@@ -72,9 +84,30 @@ void bubble_sort(void* base,
 		}
 	}
 }
+
+void test4()
+{
+	struct Stu s[3] = { {"zhang",30},{"li",34},{"wan",20} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	bubble_sort(s, sz, sizeof(s[0]), sort_age);//年龄
+	//bubble_sort(s, sz, sizeof(s[0]), sort_name);//名字
+	//print_arr(s, sz);
+
+}
 int main()
 {
 	//test1(); //按照
-	test2();
-
+	//test2();
+	test4();
 }
+
+//int main()
+//{
+//	int a = 10;
+//	char ch = 'w';
+//	void* p = &a;//无具体类型 - 可以存放任意类型
+//	p = &ch;
+//	//*p;或 p++;都不可以 - 解引用或跳过字节未知
+//	return 0;
+//}
+
