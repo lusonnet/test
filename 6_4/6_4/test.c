e624bc603068c4adc9961469de9f3e56cf85418d
sonnet <lulu_2001@foxmail.com>
2021-06-04 23:26:22 +0800

    提交代码

---
+++ 6_4/6_4/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<string.h>
+//struct Book
+//{
+//	double price;
+//	char name[31];
+//}Book;
+//
+//int main()
+//{
+//	int n = 0;
+//	char c;
+//	scanf("%d", &n);
+//	struct Book book[10];
+//	int i, max = 0, min = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%c", &c);
+//		gets(book[i].name);
+//		scanf("%lf", &book[i]);
+//		if (book[i].price > book[max].price)
+//		{
+//			max = i;
+//		}
+//		if (book[i].price < book[min].price)
+//		{
+//			min = i;
+//		}
+//	}
+//	printf("%.2lf,%s\n", book[max].price, book[max].name);
+//	printf("%.2lf,%s\n", book[min].price, book[min].name);
+//}
+
+//Ë³Ðò²éÕÒ·¨
