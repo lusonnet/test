47109c979c6acf68b1f780824a842db1bf8e7d1f
sonnet <lulu_2001@foxmail.com>
2021-06-10 21:53:23 +0800

    提交代码

--- 6_9/6_9/test.c
+++ 6_9/6_9/test.c
@@ -76,48 +76,86 @@
 
 
 
-char* my_strstr(char* str1, char* str2)
-{
-	assert(str1 && str2);
-	const char* s1 = NULL;
-	const char* s2 = NULL;
-	const char* cp = str1;
+//char* my_strstr(char* str1, char* str2)
+//{
+//	assert(str1 && str2);
+//	const char* s1 = NULL;
+//	const char* s2 = NULL;
+//	const char* cp = str1;
+//
+//	if (*str2 == '\0')
+//		return (char*)str1;
+//	while (*cp)
+//	{
+//		s1 = cp;
+//		s2 = str2;
+//		while (*s1 && *s2 && (*s1 == *s2))
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//		{
+//			return cp;
+//		}
+//		if (*s2 == '\0')
+//		{
+//			return (char*)cp;
+//		}
+//		cp++;
+//	}
+//	return NULL;
+//}
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "cdef";
+//	char* ret = my_strstr(arr1, arr2);
+//	if (ret == NULL)
+//	{
+//		printf("没找到\n");
+//	}
+//	else
+//	{
+//		printf("找到了:%s\n", ret);
+//	}
+//	return 0;
+//}
 
-	if (*str2 == '\0')
-		return (char*)str1;
-	while (*cp)
-	{
-		s1 = cp;
-		s2 = str2;
-		while (*s1 && *s2 && (*s1 == *s2))
-		{
-			s1++;
-			s2++;
-		}
-		if (*s2 == '\0')
-		{
-			return cp;
-		}
-		if (*s2 == '\0')
-		{
-			return (char*)cp;
-		}
-		cp++;
-	}
-	return NULL;
-}
+//int main()
+//{
+//	char arr[] = "lulu@foxmail.com";
+//	char* p = "@.";
+//	char tmp[20] = { 0 };
+//	strcpy(tmp, arr);
+//	char* ret = NULL;
+//	for(ret = strtok(tmp,p);ret!=NULL;ret = strtok(NULL,p))
+//	{
+//		printf("%s\n",ret);
+//	}
+//	//strtok(tmp, p);
+//	//strtok(NULL, p);
+//
+//	return 0;
+//}
+
+#include<errno.h>
 int main()
 {
-	char arr1[] = "abcdef";
-	char arr2[] = "cdef";
-	char* ret = my_strstr(arr1, arr2);
-	if (ret == NULL)
+	//printf("%s\n", strerror(0));
+	//printf("%s\n", strerror(1));
+	//printf("%s\n", strerror(2));
+	//printf("%s\n", strerror(3));
+	//printf("%s\n", strerror(4));
+	//printf("%s\n", strerror(5));
+
+	FILE* pf = fopen("text.txt", "r");
+	if (pf == NULL)
 	{
-		printf("没找到\n");
+		printf("%s\n", strerror(errno));
+		return 1;
 	}
-	else
-	{
-		printf("找到了:%s\n", ret);
-	}
+	fclose(pf);
+	pf = NULL;
 	return 0;
 }
